91a1df298cb43d4eef501861b272f49e577ddb52
gehadmohamedh <48691490+gehadmohamedh@users.noreply.github.com>
2019-11-25 14:08:44 +0200

    Add files via upload

---
+++ Piece.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    enum Type
+    {
+        sphinx,
+        pyramid,
+        scarab,
+        anubis,
+        pharoah,
+    }
+
+    class Piece
+    {
+        char rotation;
+        char color ; 
+        Type type;
+
+    }
+}
