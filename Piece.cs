56eb9a903ce9f5e6ac38a86b3ad6de7a3cad80a3
AdhamGhoname <hghoname@yahoo.com>
2019-12-06 21:15:01 +0200

    Updated.

--- Piece.cs
+++ Piece.cs
@@ -4,24 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DSALGOPROJECT
+public enum Type
 {
-    public enum Type
-    {
-        sphinx = 1,
-        pyramid = 2,
-        scarab = 3,
-        anubis = 4,
-        pharoah = 5,
-        empty = 0,
-    };
+    sphinx = 1,
+    pyramid = 2,
+    scarab = 3,
+    anubis = 4,
+    pharoah = 5,
+    empty = 0
+};
 
-    public class Piece
+
+public enum Rotation
+{
+    up = 0,
+    down = 1,
+    left = 2, 
+    right = 3
+}
+
+
+public enum Color
+{
+    silver = 0,
+    red = 1
+}
+
+public class Piece
+{
+    public Rotation rotation;
+    public Color color;
+    public Type type;
+
+    public Piece(Rotation rotation, Color color, Type type)
     {
-        public int rotation;
-        public char color;
-        public Type type;
+        this.rotation = rotation;
+        this.color = color;
+        this.type = type;
     }
 
-
+    public static Piece Clone(Piece x)
+    {
+        return new Piece(x.rotation, x.color, x.type);
+    }
 }
