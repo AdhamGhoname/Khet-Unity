0493d39827b611db9bffea5dc1c833da91dcd2e8
gehadmohamedh <48691490+gehadmohamedh@users.noreply.github.com>
2019-12-13 23:53:37 +0200

    Add files via upload

--- Piece.cs
+++ Piece.cs
@@ -4,60 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public enum Type
+namespace ConsoleApp4
 {
-    sphinx = 1,
-    pyramid = 2,
-    scarab = 3,
-    anubis = 4,
-    pharoah = 5,
-    empty = 0
-};
-
-
-public enum Value
-{
-    pyramid = 2,
-    anubis = 4,
-    pharoah = 100,
-    sphinx = 0,
-    scarab = 0,
-    empty = 0
-};
-
-public enum Rotation
-{
-    up = 0,
-    right = 1,
-    down = 2,
-    left = 3, 
-    reverse = -1
-}
-
-
-public enum Color
-{
-    silver = 0,
-    red = 1
-}
-
-public class Piece
-{
-    public Rotation rotation;
-    public Color color;
-    public Type type;
-    public Value value;
-
-    public Piece(Rotation rotation, Color color, Type type, Value value)
+    public enum Type
     {
-        this.value = value;
-        this.rotation = rotation;
-        this.color = color;
-        this.type = type;
+        sphinx = 1,
+        pyramid = 2,
+        scarab = 3,
+        anubis = 4,
+        pharoah = 5,
+        empty = 0
+    };
+
+
+    public enum Value
+    {
+        pyramid = 2,
+        anubis = 4,
+        pharoah = 100,
+        sphinx = 0,
+        scarab = 0,
+        empty = 0
+    };
+
+
+
+    public enum Rotation
+    {
+        up = 0,
+        right = 1,
+        down = 2,
+        left = 3,
+        reverse = -1
     }
 
-    public static Piece Clone(Piece x)
+
+    public enum color
     {
-        return new Piece(x.rotation, x.color, x.type,x.value);
+        silver = 0,
+        red = 1
     }
+
+
+    public class Piece
+    {
+        public Rotation rotation;
+        public color color;
+        public Type type;
+        public Value value;
+
+        public Piece(Rotation rotation, color color, Type type, Value value)
+        {
+            this.rotation = rotation;
+            this.color = color;
+            this.type = type;
+            this.value = value;
+        }
+
+        public static Piece Clone(Piece x)
+        {
+            return new Piece(x.rotation, x.color, x.type, x.value);
+        }
+
+        public static bool Compare(Piece a, Piece b)
+        {
+            if (a.rotation == b.rotation && a.value == b.value && a.color == b.color && a.type == b.type)
+                return true;
+            return false;
+        }
+    }
+
 }
